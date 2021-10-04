3a1e75e6c29e745e2a3daf682b498da6d5344dc3
Paul Wexler <paul.e.wexler@gmail.com>
2021-10-03 22:39:55 -0600

    under development

--- src/vsav/__init__.py
+++ src/vsav/__init__.py
@@ -12,29 +12,31 @@ class VersionUtil:
     """
     version suffixes: .v.nnn
     """
+    encoding = 'utf-8'
+
     @classmethod
-    def diff(cls, fname, *diffoptions):
-        """ diff `fname` `*diffoptions`
+    def diff(cls, fname, *diff_options):
+        """ diff `fname` `*diff_options`
         """
-        (aok, src, dst) = cls.get_names(fname)
-        if not aok:
-            sys.stderr.write(f'cannot find backup version of {dst}\n')
+        (specific, current) = cls.get_names(fname)
+        if specific is None:
             return
-        if not os.access(src, os.R_OK):
-            sys.stderr.write(f'cannot read {src}\n')
+        if not os.access(specific, os.R_OK):
+            sys.stderr.write(f'cannot read {specific}\n')
             return
-        os.system(f'diff {src} {dst} {" ".join(diffoptions)}')
+        os.system(f'diff {" ".join(diff_options)} {specific} {current}')
 
     @staticmethod
-    def get_highest_version(prefix):
+    def get_highest_version(fname):
         """
-        return an int: the highest version of `prefix`
+        return an int: the highest version of `fname`
         or -1 if no version is found.
         """
         highest_version = -1
+        prefix = f'{fname}.v.'
         prefix_len = len(prefix)
-        for fname in glob.glob(f'{prefix}*'):
-            suffix = fname[prefix_len:]
+        for fpath in glob.glob(f'{prefix}*'):
+            suffix = fpath[prefix_len:]
             if suffix.isdigit() and len(suffix) >= 3:
                 version = int(suffix)
                 highest_version = max(version, highest_version)
@@ -43,42 +45,43 @@ class VersionUtil:
     @classmethod
     def get_names(cls, fname):
         """
-        Returns tuple (aok, src, dst)
+        Returns tuple (specific, current)
+        `specific` has a version suffix, `current` does not.
+        `specific` is `None` when no backup version of `current` exists.
         """
-        aok = True
-        src = fname
+        specific = fname
         flist = fname.split('.')
         if (len(flist) > 2
                 and flist[-2] == 'v'
                 and flist[-1].isdigit()
                 and len(flist[-1]) >= 3):
-            dst = '.'.join(flist[0 : -2])
+            current = '.'.join(flist[0 : -2])
         else:
-            prefix = f'{fname}.v.'
-            version = cls.get_highest_version(prefix)
+            current = fname
+            version = cls.get_highest_version(fname)
             if version == -1:
-                aok = False
-            src = f'{prefix}{version:03d}'
-            dst = fname
-        return (aok, src, dst)
+                sys.stderr.write(f'cannot find backup version of {current}\n')
+                specific = None
+            else:
+                specific = f'{fname}.v.{version:03d}'
+        return (specific, current)
 
     @classmethod
     def restore(cls, fname):
         """
         Restore file from specific version or latest version.
         """
-        (aok, src, dst) = cls.get_names(fname)
-        if not aok:
-            sys.stderr.write(f'cannot find backup version of {dst}\n')
+        (specific, current) = cls.get_names(fname)
+        if specific is None:
             return
-        if not os.access(src, os.R_OK):
-            sys.stderr.write(f'cannot read {src}\n')
+        if not os.access(specific, os.R_OK):
+            sys.stderr.write(f'cannot read {specific}\n')
             return
-        with open(src, 'r', encoding="utf-8") as inf:
-            with open(dst, 'w', encoding="utf-8") as ouf:
+        with open(specific, 'r', encoding=cls.encoding) as inf:
+            with open(current, 'w', encoding=cls.encoding) as ouf:
                 ouf.write(inf.read())
-        os.utime(dst, os.stat(src)[7:9])
-        sys.stderr.write(f'{dst} restored from {src}\n')
+        os.utime(current, os.stat(specific)[7:9])
+        sys.stderr.write(f'{current} restored from {specific}\n')
 
     @classmethod
     def save(cls, fname):
@@ -88,18 +91,17 @@ class VersionUtil:
         if not os.access(fname, os.R_OK):
             sys.stderr.write(f'cannot read {fname}\n')
             return
-        prefix = '%s.v.' % fname
-        version = cls.get_highest_version(prefix) + 1
-        dst = f'{prefix}{version:03d}'
-        with open(fname, 'r', encoding="utf-8") as inf:
-            with open(dst, 'w', encoding="utf-8") as ouf:
+        version = cls.get_highest_version(fname) + 1
+        latest = f'{fname}.v.{version:03d}'
+        with open(fname, 'r', encoding=cls.encoding) as inf:
+            with open(latest, 'w', encoding=cls.encoding) as ouf:
                 ouf.write(inf.read())
-        os.utime(dst, os.stat(fname)[7:9])
-        sys.stderr.write(f'{fname} saved in {dst}\n')
+        os.utime(latest, os.stat(fname)[7:9])
+        sys.stderr.write(f'{fname} saved in {latest}\n')
 
 
 def vdif():
-    """vdif.  usage>
+    """
     Compare highest version or specific version to current version.
     """
     parser = argparse.ArgumentParser(description=vdif.__doc__)
@@ -107,27 +109,14 @@ def vdif():
             'fname',
             help='The file (or file.v.nnn) to compare)')
     parser.add_argument(
-            'diff_option',
+            'diff_options',
             nargs='*',
             help='options for diff')
     args = parser.parse_args()
-    VersionUtil.diff(args.fname, *args.diff_option)
-
-def vsav():
-    """vsav.  usage>
-    Save current version.
-    """
-    parser = argparse.ArgumentParser(description=vsav.__doc__)
-    parser.add_argument(
-            'fname',
-            nargs='+',
-            help='The file to save.')
-    args = parser.parse_args()
-    for fname in args.fname:
-        VersionUtil.save(fname)
+    VersionUtil.diff(args.fname, *args.diff_options)
 
 def vres():
-    """vres.  usage>
+    """
     Restore current version from highest version or specific version.
     """
     parser = argparse.ArgumentParser(description=vres.__doc__)
@@ -138,3 +127,16 @@ def vres():
     args = parser.parse_args()
     for fname in args.fname:
         VersionUtil.restore(fname)
+
+def vsav():
+    """
+    Save current version.
+    """
+    parser = argparse.ArgumentParser(description=vsav.__doc__)
+    parser.add_argument(
+            'fname',
+            nargs='+',
+            help='The file to save.')
+    args = parser.parse_args()
+    for fname in args.fname:
+        VersionUtil.save(fname)
